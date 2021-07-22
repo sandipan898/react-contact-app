253e6bbd7237f41636381f2236bbb586bddc1163
sandipan898 <sandipan.das898@gmail.com>
2021-07-22 11:32:13 +0530

    JSON-Server

---
+++ react-contact-app/src/components/ContactList.js
@@ -0,0 +1,45 @@
+import React, { useRef } from 'react';
+import { Link } from 'react-router-dom';
+import ContactCard from './ContactCard';
+
+const ContactList = (props) => {
+    const inputEl = useRef("");
+    const deleteContactHandler = (id) => {
+        props.getContactId(id)
+    }
+    const renderContactList = props.contacts.map((contact) => {
+        return (
+            <ContactCard contact={contact} 
+                clickHandler={deleteContactHandler}
+                key={contact.id} />
+        )
+    })
+
+    const getSearchTerm = () => {
+        props.searchKeyword(inputEl.current.value);
+    }
+    return (
+        <div className="main">
+            <h2>
+                Contact List
+                <Link to="/add">
+                    <button className="ui button blue right">Add Contact</button>
+                </Link>
+            </h2>
+            <div className="ui search">
+                <div className="ui icon input">
+                    <input type="text" placeholder="Search Contacts"
+                        ref={inputEl} 
+                        className="prompt" value={props.term}
+                        onChange={ getSearchTerm }/>
+                    <i className="search icon"></i>
+                </div>
+            </div>
+            <div className="ui celled list">
+                {renderContactList}
+            </div>
+        </div>
+    );
+}
+
+export default ContactList;
